a99b0139f22f5e27e42ee1c2a6bfb9eab0c91a1a
binmagdi <samyy.ads1@gmail.com>
2025-04-14 09:12:03 +0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="font-bold text-sky-400">Welcome to our Store</h1>
+    </div>
+  );
+}
